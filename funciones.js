ffb6a777f37dd22d83ba767ad7e5703ac99e8ef0
Edwin Cabascango <euddyk@gmail.com>
2021-04-05 17:24:03 -0500

    Reducir un array a un valor, funtion reduce

--- funciones.js
+++ funciones.js
@@ -1,35 +1,43 @@
 var sacha = {
     nombre: 'Sacha',
     apellido: 'Lifszyc',
-    altura: 1.72
+    altura: 1.72,
+    cantidadDeLibros: 111
 }
 
 var alan = {
     nombre: 'Alan',
     apellido: 'Perez',
-    altura: 1.86
+    altura: 1.86,
+    cantidadDeLibros: 78
 }
 
 var martin = {
     nombre: 'Martin',
     apellido: 'Gomez',
-    altura: 1.85
+    altura: 1.85,
+    cantidadDeLibros: 132
 }
 
 var dario = {
     nombre: 'Dario',
     apellido: 'Juarez',
-    altura: 1.71
+    altura: 1.71,
+    cantidadDeLibros: 90
 }
-var vicky={
-    nombre:'Vicky',
-    apellido:'Zapata',
-    altura:1.56
+
+var vicky = {
+    nombre: 'Vicky',
+    apellido: 'Zapata',
+    altura: 1.56,
+    cantidadDeLibros: 91
 }
-var paula={
-    nombre:'Paula',
-    apellido:'Barros',
-    altura:1.76
+
+var paula = {
+    nombre: 'Paula',
+    apellido: 'Barros',
+    altura: 1.76,
+    cantidadDeLibros: 182
 }
 /* const esAlta =persona=> persona.altura> 1.8 */
 const esAlta =({altura})=> altura> 1.8
@@ -60,5 +68,20 @@ const pasarAltutaACms = persona=> ({
     altura: persona.altura*100
 })
 var personasCms=personas.map(pasarAltutaACms)
-/* console.log(personasCms) */
-console.table(personasCms)
+
+/* var acum=0
+for (var i=0; i <personas.length; i++){
+    acum=acum+personas[i].cantidadDeLibros
+
+} */
+
+// Una forma mas prolija de hacer the above
+/* const reducer =(acum,persona)=>{
+    return acum+persona.cantidadDeLibros
+} */
+const reducer =(acum,{cantidadDeLibros})=>
+acum+cantidadDeLibros
+
+// reduce (funtion, valorInicial)
+let totalLibros = personas.reduce(reducer, 0)
+console.table(`En total todos tienen ${totalLibros} libros`)
