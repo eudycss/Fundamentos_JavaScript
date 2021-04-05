a84e781ded177c54b7408ddac519c2b501c38a39
Edwin Cabascango <euddyk@gmail.com>
2021-04-05 13:18:48 -0500

    Arrays e iteracion

--- funciones.js
+++ funciones.js
@@ -1,21 +1,40 @@
-var signo= prompt('Cual es tu digno')
-switch(signo){
-    case 'acuario':
-        console.log('Los Acuario son tímidos y callados, pero al mismo tiempo, pueden ser excéntricos y enérgicos. Sin embargo, en ambos casos, son pensadores y muy intelectuales, y aman ayudar a otros. Son capaces de ver las dos caras de una situación sin prejuicios, lo cual les permite resolver problemas.')
-    break
-
-    case 'piscis':
-        console.log('Los Piscis son muy amistosos, así que siempre los encontrarás en compañía de gente diferente. Los Piscis son abnegados, siempre tienen la disposición de ayudar a los demás, sin esperar recibir nada a cambio.')
-    break
-    case'aries':
-    case'Aries':
-        console.log('Siendo el primer signo del zodíaco, la presencia de Aries casi siempre marca el inicio de algo turbulento y enérgico. Las personas de éste signo están buscando continuamente dinamismo, velocidad y competencia. Son siempre los primeros en todo – desde el trabajo hasta en los eventos sociales.')
-    break
-
-    default:
-        console.log('No es un signo zodiacal')
-    break
-
-
+var sacha = {
+    nombre: 'Sacha',
+    apellido: 'Lifszyc',
+    altura: 1.72
 }
 
+var alan = {
+    nombre: 'Alan',
+    apellido: 'Perez',
+    altura: 1.86
+}
+
+var martin = {
+    nombre: 'Martin',
+    apellido: 'Gomez',
+    altura: 1.85
+}
+
+var dario = {
+    nombre: 'Dario',
+    apellido: 'Juarez',
+    altura: 1.71
+}
+var vicky={
+    nombre:'Vicky',
+    apellido:'Zapata',
+    altura:1.56
+}
+var paula={
+    nombre:'Paula',
+    apellido:'Barros',
+    altura:1.76
+}
+
+var personas =[sacha, alan, martin, dario,vicky,paula]
+
+for(let i =0; i <personas.length; i++){
+    let persona =personas[i]
+    console.log(`${persona.nombre} mide ${persona.altura}mts`)
+}
