0f28e48d914cad640a64f976ab7787fe5e11fc3a
Edwin Cabascango <euddyk@gmail.com>
2021-04-05 12:45:41 -0500

    Estructuras repetitivas: do-while

--- funciones.js
+++ funciones.js
@@ -1,34 +1,14 @@
-var edwin = {
-    nombre:'Edwin',
-    apellido:'Cabascango',
-    edad :28,
-    peso: 75
+var contandor =0
+// llueve le 25% de las veces
+/* var ran= Math.random()
+ */const llueve=()=>Math.random() <0.25
+do {
+ contandor++
+} while(!llueve())
+console.log("LLueve = "+llueve())
+if (contandor===1)
+{
+    console.log(`Fui a ver si llovía ${contandor} vez`)
+} else {
+    console.log(`Fui a ver si llovía ${contandor} veces`)
 }
-
-console.log(`Al inicio del año ${edwin.nombre} pesa ${edwin.peso}kg`)
-/* function aumentarDePeso(persona) {
-    persona.peso+= 200
-} */
-
-const INCREMENTO_PESO=0.3
-const DIAS_ANIO=365
-const aumentarDePeso= persona =>persona.peso+=INCREMENTO_PESO
- const adelgazar = persona => persona.peso -=INCREMENTO_PESO
-const comeMucho=()=>Math.random() <0.3
-const realizaDeporte = ()=> Math.random()<0.4
- const META = edwin.peso -3 //72
- var dias =0
-
-while(edwin.peso>META){
-if(comeMucho()){
-    //aumentar peso
-    aumentarDePeso(edwin)
-}
-if(realizaDeporte()){
-    //adelgazar
-    adelgazar(edwin)
-}
-dias +=1
-}
-console.log(`Pasaron ${dias} dias hasat que ${edwin.nombre} adelgazó 3kg `)
-/* console.log(`Al final del año ${edwin.nombre} pesa ${edwin.peso.toFixed(1)}kg`) */
