f3c8dc6b2f5d4b5092cdef8d038a2090143f4417
Edwin Cabascango <euddyk@gmail.com>
2021-04-05 18:05:57 -0500

    Prototype

--- funciones.js
+++ funciones.js
@@ -1,87 +1,28 @@
-var sacha = {
-    nombre: 'Sacha',
-    apellido: 'Lifszyc',
-    altura: 1.72,
-    cantidadDeLibros: 111
+// Prototipo
+// Es como clase, No existe la herencia, pero esto es algo similar
+
+function Persona(nombre, apellido, alt){
+    this.nombre = nombre
+    this.apellido=apellido
+    this.altura=alt
+    //return this esto lo hace implicitamente
 }
 
-var alan = {
-    nombre: 'Alan',
-    apellido: 'Perez',
-    altura: 1.86,
-    cantidadDeLibros: 78
+Persona.prototype.saludar= function(){
+    console.table(`Hola me llamo ${this.nombre} ${this.apellido}`)
 }
-
-var martin = {
-    nombre: 'Martin',
-    apellido: 'Gomez',
-    altura: 1.85,
-    cantidadDeLibros: 132
-}
-
-var dario = {
-    nombre: 'Dario',
-    apellido: 'Juarez',
-    altura: 1.71,
-    cantidadDeLibros: 90
-}
-
-var vicky = {
-    nombre: 'Vicky',
-    apellido: 'Zapata',
-    altura: 1.56,
-    cantidadDeLibros: 91
-}
-
-var paula = {
-    nombre: 'Paula',
-    apellido: 'Barros',
-    altura: 1.76,
-    cantidadDeLibros: 182
-}
-/* const esAlta =persona=> persona.altura> 1.8 */
-const esAlta =({altura})=> altura> 1.8
-const esBaja =({altura})=> altura <1.8
-var personas =[sacha, alan, martin, dario,vicky,paula]
-// vamos a filtrar a las personas altas
-
-var personasAltas = personas.filter(esAlta)
-var personasBajas=personas.filter(esBaja)
-// Esta funcion si modifica al array original
-//const pasarAltutaACms=(persona)=>{
-    /* persona.altura= persona.altura*100 */
-/*     persona.altura *=100
-    return persona
-} */
-
-// Esta funcion no modifica el array original
-/* const pasarAltutaACms = persona=>{
-    return{
-        ...persona,
-        altura: persona.altura*100
+/* let alt1= Persona.this.altura
+console.table("Altura:"+alt1) */
+Persona.prototype.esAlto= function(){
+    if(this.altura>1.8){
+        console.table(`Hola me llamo ${this.nombre} ${this.apellido} y soy alto`)
+    } else{
+        console.table(`Hola me llamo ${this.nombre} ${this.apellido} y no soy alto`)
     }
-} */
+    
+}
 
-// The same funtion above but without return, use parentesis
-const pasarAltutaACms = persona=> ({
-    ...persona,
-    altura: persona.altura*100
-})
-var personasCms=personas.map(pasarAltutaACms)
-
-/* var acum=0
-for (var i=0; i <personas.length; i++){
-    acum=acum+personas[i].cantidadDeLibros
-
-} */
-
-// Una forma mas prolija de hacer the above
-/* const reducer =(acum,persona)=>{
-    return acum+persona.cantidadDeLibros
-} */
-const reducer =(acum,{cantidadDeLibros})=>
-acum+cantidadDeLibros
-
-// reduce (funtion, valorInicial)
-let totalLibros = personas.reduce(reducer, 0)
-console.table(`En total todos tienen ${totalLibros} libros`)
+var sacha = new Persona('Edwin', 'Cabascango',1.92)
+var fredy= new Persona('Fredy', 'Salazar',1.6)
+var cury= new Persona('Cury','Cushcagua', 1,3)
+sacha.saludar()
