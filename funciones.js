b474332a3c8485d02e65d7fd96737bcf3152e3a8
Edwin Cabascango <euddyk@gmail.com>
2021-04-06 15:14:36 -0500

    Funciones como parámetros

--- funciones.js
+++ funciones.js
@@ -5,8 +5,15 @@ constructor(nombre, apellido, altura){
     this.apellido=apellido
     this.altura=altura
 }
-saludar(){
-    console.table(`Hola me llamo ${this.nombre} ${this.apellido}`)
+saludar(fn){
+    /* let nombre=this.nombre
+    let apellido=this.apellido */
+    /*  otra forma de lo anterior*/
+    let {nombre, apellido}=this
+    console.table(`Hola me llamo ${nombre} ${apellido}`)
+    if(fn){
+        fn(nombre,apellido)
+    }
 }
 esAlto(){
     return this.altura >1.8
@@ -20,14 +27,26 @@ class Desarrollador extends Persona{
         super(nombre,apellido,altura)  
     }
 
+ saludar(fn){
+    let {nombre, apellido}=this
+    console.table(`Hola, me llamo ${nombre} ${apellido} Soy un Desarrollador`)
+    if(fn){
+        fn(nombre,apellido,true)
+    }
+ }
 }
 
-Desarrollador.prototype.saludar=function(){
-    console.table(`Hola, me llamo ${this.nombre} ${this.apellido} Soy un Desarrollador`)
+function responderSaludo(nombre, apellido, esDev){
+    console.table(`Buen dia ${nombre} ${apellido}`)
+    if(esDev){
+        console.table(`Ah mira, no sabia que eres desarrollor/a`)
+    }
 }
 
 
 var sacha = new Persona('Edwin', 'Cabascango',1.92)
 var fredy= new Persona('Fredy', 'Salazar',1.6)
-var cury= new Persona('Cury','Cushcagua', 1,3)
-sacha.saludar()
+var cury= new Desarrollador('Cury','Cushcagua', 1,3)
+sacha.saludar() 
+fredy.saludar(responderSaludo)
+cury.saludar(responderSaludo)
