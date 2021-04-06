d927b922ae776fa72ff055d5f3947ed3d00603e2
Edwin Cabascango <euddyk@gmail.com>
2021-04-05 20:20:46 -0500

    Herencia Prototipal

--- funciones.js
+++ funciones.js
@@ -1,3 +1,16 @@
+// para hacer una herencia, tenemos que escribir una funtion propia  nuestra
+
+function heredaDe(prototipoHijo, prototipoPadre){
+    // quien va a ser su prototipo padre
+    let fn = function(){} // es funcion vacia noop
+    // le asignamos un prototitpo padre
+    fn.prototype=prototipoPadre.prototype
+    //AL P hijo le decimos que su prototipe es un nuevo objeto de fn
+    prototipoHijo.prototype = new fn
+    // Funcion constructora
+    prototipoHijo.prototype.constructor = prototipoHijo 
+}
+
 // Prototipo
 // Es como clase, No existe la herencia, pero esto es algo similar
 
@@ -18,6 +31,16 @@ Persona.prototype.esAlto=function () {
     return this.altura >1.8
     
 }
+
+function Desarrollador(nombre,apellido){
+    this.nombre=nombre
+    this.apellido=apellido
+}
+heredaDe(Desarrollador,Persona)
+Desarrollador.prototype.saludar=function(){
+    console.table(`Hola, me llamo ${this.nombre} ${this.apellido} Soy un Desarrollador`)
+}
+
 
 var sacha = new Persona('Edwin', 'Cabascango',1.92)
 var fredy= new Persona('Fredy', 'Salazar',1.6)
