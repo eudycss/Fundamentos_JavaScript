320826955dcbd4d0ca4d7af2b0990bbc740ba7be
Edwin Cabascango <euddyk@gmail.com>
2021-04-06 19:39:10 -0500

    Manejando el Orden y el Asincronismo en JavaScript, callback hell

--- funciones.js
+++ funciones.js
@@ -3,16 +3,33 @@ const PEOPLE_URL='people/:id'
 /* const lukeUrl=`${API_URL}${PEOPLE_URL.replace(':id',1)}`
  */
 const opts ={crossDomain:true}
-const onPeopleResponse=function(persona){
-    console.log(`Hola yo soy, ${persona.name}`)
-}
 
-function obtenerPersonaje(id){
+
+function obtenerPersonaje(id,callback){
+    console.log(id)
     const url =`${API_URL}${PEOPLE_URL.replace(':id',id)}`
-    $.get(url, opts,onPeopleResponse)
+    $.get(url, opts,function(persona){
+        console.log(`Hola yo soy, ${persona.name}`)
+        if(callback){
+            callback()
+        }
+    })
 }
-obtenerPersonaje(1)
-obtenerPersonaje(2)
+//callback hell
+obtenerPersonaje(1, function(){
+    obtenerPersonaje(2,function(){
+        obtenerPersonaje(3,function(){
+            obtenerPersonaje(4,function(){
+                obtenerPersonaje(5,function(){
+                    obtenerPersonaje(6,function(){
+                        obtenerPersonaje(7)
+                    })
+                })
+            })
+        })
+    })
+})
+
 obtenerPersonaje(3)
  // primer parametro
 // segundo parametro
